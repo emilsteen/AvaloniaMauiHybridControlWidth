49b4590db6646db741284ea77595800acbd1e1f3
emil.steen <emil@cognitioab.se>
2025-09-29 20:30:41 +0200

    Added Maui Application from template.

---
+++ MauiAvaloniaApplication/MauiAvaloniaApplication/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAvaloniaApplication
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
